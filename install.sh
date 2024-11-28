69373ef42fc51447f7a586bee63fa86274f6f5a8
José Miguel Talavera Uribe <127774672+MakiMike@users.noreply.github.com>
2024-11-28 11:50:11 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget "https://e7.pngegg.com/pngimages/677/744/png-clipart-calligraphy-happy-hand-sunlight.png"
+mv https://e7.pngegg.com/pngimages/677/744/png-clipart-calligraphy-happy-hand-sunlight.png /home/quetzal/Pictures/https://e7.pngegg.com/pngimages/677/744/png-clipart-calligraphy-happy-hand-sunlight.png /home/quetzal/Pictures
+sudo apt install php -y
+php -v
+ 
